5fb8247e6b4cb0087690493cdd83cc8f0c123189
banumbas <banumbas1@gmail.com>
2026-08-11 05:45:08 +0300

    new brif sound, inhand sawn pmc, 26 viewport

--- Content.Server/GameTicking/Rules/Components/NukeopsRuleComponent.cs
+++ Content.Server/GameTicking/Rules/Components/NukeopsRuleComponent.cs
@@ -93,7 +93,7 @@ public sealed partial class NukeopsRuleComponent : Component
     public WinType WinType = WinType.Neutral;
 
     [DataField]
-    public List<WinCondition> WinConditions = new ();
+    public List<WinCondition> WinConditions = new();
 
     [DataField]
     public EntityUid? TargetStation;
@@ -105,7 +105,7 @@ public sealed partial class NukeopsRuleComponent : Component
     ///     Path to antagonist alert sound.
     /// </summary>
     [DataField]
-    public SoundSpecifier GreetSoundNotification = new SoundPathSpecifier("/Audio/Ambience/Antag/nukeops_start.ogg");
+    public SoundSpecifier GreetSoundNotification = new SoundPathSpecifier("/Audio/_Classic/Ambience/Antag/pmc_start.ogg");
 }
 
 public enum WinType : byte
