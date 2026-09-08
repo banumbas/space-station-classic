6b7634cbd5b094de4cd1828ce599c23e2842eb0f
banumbas <banumbas1@gmail.com>
2026-09-08 16:01:40 +0300

    визоры техники и перископы

---
+++ Content.Shared/_Classic/Vehicles/Core/VehicleViewToggleComponents.cs
@@ -0,0 +1,39 @@
+using Content.Shared._Classic.Vehicles;
+using System.Collections.Generic;
+using Content.Shared.Actions;
+using Robust.Shared.GameObjects;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._Classic.Vehicles;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(VehicleViewToggleSystem))]
+public sealed partial class VehicleViewToggleComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public EntProtoId ActionId = "ActionVehicleToggleView";
+
+    [DataField, AutoNetworkedField]
+    public EntityUid? Action;
+
+    [DataField, AutoNetworkedField]
+    public EntityUid? OutsideTarget;
+
+    [DataField, AutoNetworkedField]
+    public EntityUid? InsideTarget;
+
+    [DataField, AutoNetworkedField]
+    public EntityUid? Source;
+
+    [DataField, AutoNetworkedField]
+    public bool IsOutside;
+
+    [DataField, AutoNetworkedField]
+    public HashSet<EntityUid> Sources = new();
+}
+
+public sealed partial class VehicleToggleViewActionEvent : InstantActionEvent;
+
+[ByRefEvent]
+public readonly record struct VehicleViewToggledEvent(bool IsOutside);
